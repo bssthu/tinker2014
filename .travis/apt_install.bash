60064dd97386feb03a9d6ce9d0717937c16d1be9
bssthu <bssda03@gmail.com>
2015-03-20 09:40:33 +0800

    don't install liblz-4-dev from apt-get

--- .travis/apt_install.bash
+++ .travis/apt_install.bash
@@ -10,5 +10,4 @@ sudo apt-get install -qq -y python-wstool python-rosinstall
 sudo apt-get install -qq -y python-empy python-nose python-sip-dev
 sudo apt-get install -qq -y libpoco-dev libtinyxml-dev
 sudo apt-get install -qq -y build-essential
-sudo apt-get install -qq -y liblz4-dev
 
