5d09877d163e2396ceedcaae49f460faf7c88856
bssthu <bssda03@gmail.com>
2015-03-20 21:32:00 +0800

    install cv 2.4.9 from source

--- .travis/install_cv.bash
+++ .travis/install_cv.bash
@@ -9,7 +9,17 @@ export CI_SOURCE_PATH=$(pwd)
 # compile opencv
 cd $HOME
 
-sudo apt-get install -qq -y libopencv-dev
+mkdir cv
+cd cv/
+wget http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.9/opencv-2.4.9.zip
+unzip -qq opencv-2.4.9.zip
+cd opencv-2.4.9/
+mkdir build
+cd build/
+cmake ..
+make -j4
+sudo make install
+sudo ldconfig
 
 cd $CI_SOURCE_PATH
 
