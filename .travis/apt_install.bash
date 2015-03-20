ca3935110ec01cbde635ddb2668e9db4dfd4e289
bssthu <bssda03@gmail.com>
2015-03-20 17:59:03 +0800

    install libqt4-dev

--- .travis/apt_install.bash
+++ .travis/apt_install.bash
@@ -21,5 +21,5 @@ sudo apt-get install -qq -y python-empy python-nose python-sip-dev
 sudo apt-get install -qq -y libpoco-dev libtinyxml-dev
 sudo apt-get install -qq -y liblz4-dev
 sudo apt-get install -qq -y build-essential
-sudo apt-get install -qq -y pyqt4-dev-tools
+sudo apt-get install -qq -y pyqt4-dev-tools libqt4-dev
 
