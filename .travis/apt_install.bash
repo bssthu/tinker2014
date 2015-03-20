3ccbd040ab865d987b25f493a1f3c3eca01c12a5
bssthu <bssda03@gmail.com>
2015-03-20 19:12:17 +0800

    install gtest

--- .travis/apt_install.bash
+++ .travis/apt_install.bash
@@ -19,7 +19,7 @@ sudo apt-get install -qq -y python-rosdep python-rosinstall-generator
 sudo apt-get install -qq -y python-wstool python-rosinstall
 sudo apt-get install -qq -y python-empy python-nose python-sip-dev
 sudo apt-get install -qq -y libpoco-dev libtinyxml-dev
-sudo apt-get install -qq -y liblz4-dev
+sudo apt-get install -qq -y liblz4-dev libgtest-dev
 sudo apt-get install -qq -y build-essential
 sudo apt-get install -qq -y pyqt4-dev-tools libqt4-dev python-qt4-dev
 
