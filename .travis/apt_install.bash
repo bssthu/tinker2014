080623ff98069f7d7de6c20d15a8a2ac641c5fd7
bssthu <bssda03@gmail.com>
2015-03-20 12:57:16 +0800

    remove space in add-apt-repository

--- .travis/apt_install.bash
+++ .travis/apt_install.bash
@@ -6,7 +6,7 @@
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
-sudo add-apt-repository ppa: gezakovacs/lz4
+sudo add-apt-repository ppa:gezakovacs/lz4
 
 sudo apt-get update -qq
 
