a58a5f33112fcc61380aae96a272da7823fe4128
bssthu <bssda03@gmail.com>
2015-03-21 11:29:55 +0800

    do not download anything from googledrive

--- .travis/console_bridge.bash
+++ .travis/console_bridge.bash
@@ -11,11 +11,11 @@ mkdir -p projects
 cd projects
 
 # for console_bridge in 12.04
-wget --no-check-certificate 'https://googledrive.com/host/0B-kQc2-wuHntUkJUZWQzd0d4Zk0' -O console_bridge.tar.gz
-tar -xzf console_bridge.tar.gz
-if [ ! $? -eq 0 ]; then
+#wget --no-check-certificate 'https://googledrive.com/host/0B-kQc2-wuHntUkJUZWQzd0d4Zk0' -O console_bridge.tar.gz
+#tar -xzf console_bridge.tar.gz
+#if [ ! $? -eq 0 ]; then
     git clone https://github.com/ros/console_bridge.git
-fi
+#fi
 
 cd console_bridge/
 mkdir -p build
@@ -26,11 +26,11 @@ sudo make install
 cd ../..
 
 # urdfdom_headers
-wget --no-check-certificate 'https://googledrive.com/host/0B-kQc2-wuHntaWVrUlBnV1dnS0' -O urdfdom_headers.tar.gz
-tar -xzf urdfdom_headers.tar.gz
-if [ ! $? -eq 0 ]; then
+#wget --no-check-certificate 'https://googledrive.com/host/0B-kQc2-wuHntaWVrUlBnV1dnS0' -O urdfdom_headers.tar.gz
+#tar -xzf urdfdom_headers.tar.gz
+#if [ ! $? -eq 0 ]; then
     git clone https://github.com/ros/urdfdom_headers.git
-fi
+#fi
 
 cd urdfdom_headers/
 mkdir -p build
@@ -40,11 +40,11 @@ sudo make install
 cd ../..
 
 # urdfdom
-wget --no-check-certificate 'https://googledrive.com/host/0B-kQc2-wuHntWTI1X2VDN1ZCQUU' -O urdfdom.tar.gz
-tar -xzf urdfdom.tar.gz
-if [ ! $? -eq 0 ]; then
+#wget --no-check-certificate 'https://googledrive.com/host/0B-kQc2-wuHntWTI1X2VDN1ZCQUU' -O urdfdom.tar.gz
+#tar -xzf urdfdom.tar.gz
+#if [ ! $? -eq 0 ]; then
     git clone https://github.com/ros/urdfdom.git
-fi
+#fi
 
 cd urdfdom
 mkdir -p build
